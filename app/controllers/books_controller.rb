fc3e9ebc788d43b591b6d054a81a9316462baef1
EC2 Default User <ec2-user@ip-172-31-30-197.ap-northeast-1.compute.internal>
2023-01-09 03:49:06 +0000

    0109start

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- db/migrate/20230101092411_create_models.rb
+++ db/migrate/20230101092411_create_models.rb
@@ -1,7 +1,8 @@
 class CreateModels < ActiveRecord::Migration[6.1]
   def change
     create_table :models do |t|
-
+      t.string :title
+      t.string :body
       t.timestamps
     end
   end
